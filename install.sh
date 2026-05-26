6ba36eee839243aabc19a61858e4e3be5b04cd91
prudhvi <prudhvi4u.s.com>
2026-05-26 12:28:29 +0530

    install mysql

--- install.sh
+++ install.sh
@@ -7,3 +7,14 @@ if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
 fi
+}
+# echo "I am continuing..."
+dnf list installed mysql
+
+if [ $? -eq 0 ]; then
+    echo "MySQL is already installed ... SKIPPING"
+else
+    echo "Installing MySQL"
+    dnf install mysql -y
+    VALIDATE MySQL $?
+fi
