90d09dd9ae0a569c8ede766ca1ffe42ab0080cc4
prudhvi <prudhvi4u.s.com>
2026-05-26 11:39:18 +0530

    conversation

--- conversation.sh
+++ conversation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PERSON1=Russia
-PERSON2=Ukraine
+PERSON1=prudhvi
+PERSON2=priyanka
 
-echo "$PERSON1: Hey Ukraine, we are going blast you!"
+echo "$PERSON1: Hey $PERSON2, we are going blast you!"
 echo "$PERSON2: Yes, please. you are welcome"
 echo "$PERSON1: Hey $PERSON2 I am serious"
 echo "$PERSON2: Hey $PERSON1, we are also serious"
